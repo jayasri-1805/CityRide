6baf8b4835e7d67cb71b0ee458964bdbd8b54d7f
Jayasri Patnala <jayasripatnala11@gmail.com>
2025-09-22 22:51:13 +0530

    Update firestore.js

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,16 +1,10 @@
 import admin from "firebase-admin";
-import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
-
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-};
+const serviceAccount = path.resolve("./key.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
