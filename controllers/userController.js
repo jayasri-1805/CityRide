b7268ac2d7b9f0ece1f8f5a759765cc3dc5d462f
Jayasri Patnala <jayasripatnala11@gmail.com>
2025-09-22 22:55:11 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,14 @@ import db from "../utils/firebase.js";
 export const getProfile = async (req, res) => {
   try {
     const userRef = db.collection("users").doc(req.user.email);
-    const doc = await userRef.get();
-    if (!doc.exists) return res.status(404).json({ message: "User not found" });
-    const { password, ...data } = doc.data();
-    res.json(data);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) return res.status(404).json({ error: "User not found" });
+
+    const { name, email, role } = userDoc.data();
+    res.json({ name, email, role });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
